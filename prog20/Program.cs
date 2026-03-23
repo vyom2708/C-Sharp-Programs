59d0d43d5b74bd557270bd63361a0e7073fa778d
vyom2708 <vyompurohit.133430@marwadiuniversity.ac.in>
2026-03-23 10:29:12 +0530

    Add files via upload

---
+++ prog20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r1 = new rectangle();
+            r1.cal();
+            r1.dis();
+            rectangle r2 = new rectangle(30,40);
+            r2.cal();
+            r2.dis();
+        }
+    }
+}
---
+++ prog20/rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog20
+{
+    internal class rectangle
+    {
+        int l, w, area;
+
+        public rectangle()
+        {
+            l = 10;w = 20;
+        }
+
+        public rectangle(int l1, int w1)
+        {
+            l=l1;
+            w=w1;
+        }
+
+        public void cal()
+        {
+            area = l * w;
+        }
+
+        public void dis()
+        {
+            Console.WriteLine("AREA: " + area);
+        }
+    }
+}
